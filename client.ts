c4ab697c0c165003f61e191e3b4fb9b376bdeb8f
Vũ Lê <lehoangvu@bacoor.co>
2022-07-19 11:56:08 +0700

    update deadline

--- client.ts
+++ client.ts
@@ -1,20 +1,38 @@
-import path from "path"
-import * as grpc from "@grpc/grpc-js"
-import * as protoLoader from "@grpc/proto-loader"
-import { ProtoGrpcType } from "./proto/customer"
+import path from "path";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import { ProtoGrpcType } from "./proto/customer";
 
-const PORT = 5001
-const PROTO_FILE = './proto/customer.proto'
+const PORT = 5001;
+const PROTO_FILE = "./proto/customer.proto";
 
-const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE))
-const grpcObj = (grpc.loadPackageDefinition(packageDef) as unknown) as ProtoGrpcType
+const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE));
+const grpcObj = grpc.loadPackageDefinition(
+  packageDef
+) as unknown as ProtoGrpcType;
 
-const client = new grpcObj.customer.Customer("localhost:5001", grpc.credentials.createInsecure())
+const deadline = new Date();
+deadline.setSeconds(deadline.getSeconds() + 10);
 
-client.GetCustomerInfo({ userId: 1 }, (err, result) => {
-    if(err) {
-        console.error(err)
-        return
+const client = new grpcObj.customer.Customer(
+  `localhost:${PORT}`,
+  grpc.credentials.createInsecure()
+);
+
+client.waitForReady(deadline, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  onClientReady();
+});
+
+const onClientReady = () => {
+  client.GetCustomerInfo({ userId: 2 }, (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-    console.log(result)
-})
+    console.log(result);
+  });
+};
